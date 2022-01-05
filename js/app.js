928d838fe5d587f4999fbaff7094c589ebf458f1
Andriiduk <Andduk27@gmail.com>
2022-01-05 12:43:29 +0200

    first commit

--- js/app.js
+++ js/app.js
@@ -19,11 +19,12 @@ tl.to('#Vector_13', { scale: 0.1, x: 25, y: 26 }, '<50%')
 tl.fromTo('#Vector_12', { scale: 0, x: 0, y: 0 }, { scale: 0.3, x: -50, y: 50 }, '<')
 tl.to('#TXT', { opacity: 0, y: -10 }, '<')
 tl.to('.logo__more-2', { display: "block", ease: 'elastic.out(1, 0.4)', delay: .1 }, '<50%')
+tl.to('.logo__more-2', { opacity: 0, duration: .01 })
+tl.to('.logo__more', { display: "block", ease: 'elastic.out(1, 1)' })
 tl.to('#Vector_13', { opacity: 0, duration: .2 })
 tl.to('#Vector_12', { opacity: 0, duration: .2 }, '<')
-tl.to('#Vector_13', { opacity: 1, })
-tl.to('#Vector_12', { opacity: 1, }, '<')
-tl.to('.logo__more-2', { opacity: 0, duration: .01 })
-tl.to('.logo__more', { display: "block" }, '<')
+tl.to('#Vector_13', { opacity: 1 })
+tl.to('#Vector_12', { opacity: 1 }, '<')
+
 
 
